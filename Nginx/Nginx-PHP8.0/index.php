1439a56d4de6172a28a57c2dd799ea63b059b50f
Rodrigo Prado <rodrigo.prado@neocantra.com>
2021-10-27 18:37:22 -0300

    dockerfile nginx php

---
+++ Nginx/Nginx-PHP8.0/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>PHP Info</title>
+		<meta charset="UTF-8">
+	</head>
+	<body>
+		<h1 style="color:red; text-align:center;font-size:500%;"><b>-=| Nginx PHP Info |=-</b></h1>
+		<p style="color:white; text-align:center;font-size:200%;"><i>Nginx Index PHP Info</i></p>
+		<hr>
+		<p> 
+			<?php 
+				phpinfo(); 
+			?> 
+		</p>
+	</body>
+</html>
